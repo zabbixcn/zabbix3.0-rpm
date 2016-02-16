d3bed95b44452385e953477f5f83720a22c24bd1
itnihao <itnihao@qq.com>
2016-02-16 21:11:09 +0800

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -8,3 +8,5 @@ rpm -ivh http://repo.zabbix.com/non-supported/rhel/6/x86_64/iksemel-1.4-2.el6.x8
 rpm -ivh http://repo.zabbix.com/non-supported/rhel/6/x86_64/iksemel-devel-1.4-2.el6.x86_64.rpm
 rpm -ivh http://repo.zabbix.com/non-supported/rhel/6/x86_64/iksemel-utils-1.4-2.el6.x86_64.rpm
 wget http://repo.zabbix.com/zabbix/3.0/rhel/7/SRPMS/zabbix-3.0.0-1.el7.src.rpm
+adduser admin
+cp zabbix-3.0.0-1.el7.src.rpm /home/admin
