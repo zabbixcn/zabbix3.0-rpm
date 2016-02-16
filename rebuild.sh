be04faca61b89581491464d6ed5221d9609f2382
itnihao <itnihao@qq.com>
2016-02-16 21:12:33 +0800

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,7 @@
 yum install -y git rpm-build gcc make mysql-devel openldap-devel libssh2-devel net-snmp-devel curl-devel unixODBC-devel OpenIPMI-devel java-devel postgresql-devel gnutls-devel sqlite-devel libxml2-devel
 git clone  https://github.com/zabbixcn/curl-rpm.git
 cd curl-rpm/RPMS
-rpm -ivh curl-7.29.0-25.el6.x86_64.rpm 
-rpm -ivh libcurl-7.29.0-25.el6.x86_64.rpm
-rpm -ivh libcurl-devel-7.29.0-25.el6.x86_64.rpm
+rpm -ivh curl-7.29.0-25.el6.x86_64.rpm libcurl-7.29.0-25.el6.x86_64.rpm libcurl-devel-7.29.0-25.el6.x86_64.rpm
 rpm -ivh http://repo.zabbix.com/non-supported/rhel/6/x86_64/iksemel-1.4-2.el6.x86_64.rpm
 rpm -ivh http://repo.zabbix.com/non-supported/rhel/6/x86_64/iksemel-devel-1.4-2.el6.x86_64.rpm
 rpm -ivh http://repo.zabbix.com/non-supported/rhel/6/x86_64/iksemel-utils-1.4-2.el6.x86_64.rpm
